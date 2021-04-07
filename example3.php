9ad71a642412e7af8350d2602bd5a79b0bd77129
Austin Rippee <austinrippee@gmail.com>
2021-04-07 12:07:13 -0400

    added function and php code

---
+++ example3.php
@@ -0,0 +1,13 @@
+<?php
+echo "<a target='_blank' href='https://github.com/AustinRippee/api.git'>GitHub repo</a> ";
+// Covid19api.com deaths data
+
+main();
+
+function blah(){
+    echo "hello";
+}
+
+function blahblah(){
+    echo "blah";
+}
